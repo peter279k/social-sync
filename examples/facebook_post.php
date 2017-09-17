5dced47eb0e5ad87fe319ab027b95d98db7696db
peter279k <peter279k@gmail.com>
2017-09-18 02:38:06 +0800

    add .git hidden and facebook_post processing

--- examples/facebook_post.php
+++ examples/facebook_post.php
@@ -2,7 +2,20 @@
 /*
 * This PHP program is to post the feed via Facebook graph API.
 */
+session_start();
+
+$key = parse_ini_file(__DIR__.'/../api-key.ini');
+$appId = $key['app_id'];
+$appSecret = $key['app_secret'];
+$userToken = $key['user_access_token'];
+$fb = new \Facebook\Facebook([
+    'app_id' => $appId,
+    'app_secret' => $appSecret,
+    'default_graph_version' => 'v2.10',
+]);
 
 $response = $fb->post('/me/feed', [
     'message' => 'Fooooo message is from PHP Graph SDKv5',
 ]);
+
+var_dump($response);
