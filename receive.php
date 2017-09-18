b81b761ade5c2d5341bb9422caf38511851b83d0
peter279k <peter279k@gmail.com>
2017-09-19 04:49:11 +0800

    check the post value

--- receive.php
+++ receive.php
@@ -9,26 +9,7 @@ use peter\social\PostFeed;
 
 header("Content-type: text/plain");
 
-$message = '';
-$email = __DIR__.'/api-key.ini';
-$toEmailAddress = parse_ini_file($email)['to_email_address'];
-$fromEmailAddress = parse_ini_file($email)['from_email_address'];
-$from = isset($_POST['envelope']['from']) ? $_POST['envelope']['from']:'no-from-address';
-$to = isset($_POST['envelope']['to']) ? $_POST['envelope']['to']:'no-to-address';
-$subject = isset($_POST['headers']['Subject']) ? $_POST['headers']['Subject']:'no-subject';
-$plain = isset($_POST['plain']) ? $_POST['plain']:'no-plain-text';
-$html = isset($_POST['html']) ? $_POST['html']:'no-html-string';
-$reply = isset($_POST['reply_plain']) ? $_POST['reply_plain']:'no-reply-plain';
-
-if($to !== $toEmailAddress) {
-    echo 'the to email address is not allowed here';
-    exit;
-}
-
-if($from !== $fromEmailAddress) {
-    echo 'the from email address is not allowed here';
-    exit;
-}
+var_dump($_POST);
 
 echo 'it is successfully received by'.$fromEmailAddress.'!';
 // receive and validate the email source(CloudMailin)
